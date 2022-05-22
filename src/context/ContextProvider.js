292625a00e5cadf58cb537ca5dd07bd91de96d24
gratestas <stasgrate@gmail.com>
2022-05-22 13:10:24 +0300

    added states

--- src/context/ContextProvider.js
+++ src/context/ContextProvider.js
@@ -11,12 +11,43 @@ const initialState = {
 
 export const ContexProvider = ({ children }) => {
   const [activeMenu, setActiveMenu] = useState(true);
+  const [isClicked, setIsClicked] = useState(initialState);
+  const [themeSettings, setThemeSettings] = useState(false);
+  const [screenSize, setScreenSize] = useState(undefined);
+  const [currenMode, setCurrenMode] = useState("Light");
+  const [currentColor, setCurrentColor] = useState("#03C9D7");
+
+  const setColor = (color) => {
+    setCurrentColor(color);
+    localStorage.setItem("colorMode", color);
+  };
+
+  const setMode = (e) => {
+    setCurrenMode(e.target.value);
+    localStorage.setItem("themeMode", e.target.value);
+  };
+
+  const handleClick = (clicked) =>
+    setIsClicked({ ...initialState, [clicked]: true });
 
   return (
     <StateContext.Provider
       value={{
         activeMenu,
         setActiveMenu,
+        isClicked,
+        setIsClicked,
+        themeSettings,
+        setThemeSettings,
+        screenSize,
+        setScreenSize,
+        currenMode,
+        setCurrenMode,
+        currentColor,
+        setCurrentColor,
+        handleClick,
+        setMode,
+        setColor,
       }}
     >
       {children}
