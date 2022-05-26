6cbbb5cb977f022ce1cef86ef6945b282a47a242
gratestas <stasgrate@gmail.com>
2022-05-26 21:29:46 +0300

    added comment

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -5,7 +5,7 @@ const secret = "secret";
 const auth = async (req, res, next) => {
   try {
     const token = req.headers.authorization.split(" ")[1];
-    const isCustomAuth = token.length < 500;
+    const isCustomAuth = token.length < 500; // 2 tokens : google and mine
 
     let decodedData;
 
