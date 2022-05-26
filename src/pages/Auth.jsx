2da2af588b0bbaf7f174eb1fd1fa677fdca7639a
gratestas <stasgrate@gmail.com>
2022-05-27 00:29:00 +0300

    completed auth page

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,52 +1,57 @@
 import { useState } from "react";
-import { useUserContext } from "../context/UserContext";
-import { useNavigate } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
+
+import Input from "../components/Input/Input"
+import Button from "../components/Button";
+
 const initialState = { firstName: "", lastName: "", email: "", password: "" };
 
 const Auth = () => {
   const [form, setForm] = useState(initialState);
   const [isSignup, setIsSignup] = useState(true);
-  const { signup, signin } = useUserContext();
-  const navigate = useNavigate();
+  const { signup, signin } = useAuth();
+
+  const greeting = isSignup ? "Create an account" :" Welcome back";
+  const cta = isSignup ? "Let's get started with your 30-days free trial!" :"Welcome back! Please enter your details";
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(form);
-    if (isSignup) {
-      signup(form, navigate);
-    } else {
-      signin(form, navigate);
-    }
+    isSignup ? signup(form):signin(form);
   };
 
   const handleChange = (e) => {
     setForm({ ...form, [e.target.name]: e.target.value });
   };
   return (
-    <div className="container flex justify-center">
+    <div className=" flex justify-center mt-20">
       <div>
         <form
-          className="w-[30rem] h-[30rem] bg-red-200"
+          className="w-[25rem] xl:w-[30rem] h-[30rem] rounded-2xl  px-4 "
           onSubmit={handleSubmit}
         >
-          <div className="flex justify-between">
-            <div>
-              <label htmlFor="firstName">First Name</label>
-              <input type="text" name="firstName" onChange={handleChange} />
+          <h1 className="text-3xl xl:text-4xl font-bold tracking-tight mt-20 mb-2">{greeting}</h1>
+          <h4 className="text-sm font-medium  mb-5 text-gray-500">{cta}</h4>
+          <div className="flex flex-col flex-wrap  ">
+            {isSignup && (<div className="flex justify-between">
+            <Input name="firstName" label="First Name"  oncChange={handleChange} half/>
+            <Input name="lastName" label="Last Name"  oncChange={handleChange} half/>
+            </div>)}
+            <Input name="email" label="Email"  oncChange={handleChange}/>
+            <Input name="password" label="Password" oncChange={handleChange}/>
+          </div>
+          <div className="flex justify-end mt-3 mb-6 text-sm font-semibold cursor-pointer">
+            Forgot password
             </div>
-            <div>
-              <label htmlFor="lastName">Last Name</label>
-              <input type="text" name="lastName" onChange={handleChange} />
-            </div>
-          </div>
-          <div className="flex flex-col mt-4">
-            <label htmlFor="email">Email</label>
-            <input type="text" name="email" onChange={handleChange} />
-          </div>
-          <div className="flex flex-col mt-4">
-            <label htmlFor="password">Password</label>
-            <input type="text" name="password" onChange={handleChange} />
-          </div>
-          <button type="submit">Submit</button>
+          <Button color="white" bgColor="black" borderRadius="10px" size="md" text={ isSignup ? "Sign up" : "Sing in"}/>
+          <div className="flex justify-center mt-6 text-sm font-semibold text-gray-400">
+          {isSignup ? "Already have an account?" :"Don't have an account?"}
+          <span 
+          className="ml-2 text-bold text-gray-900 cursor-pointer"
+          onClick={() => setIsSignup((prevState)=>!prevState)}
+          >
+            { !isSignup ? "Sign up" : "Sing in" }
+            </span>
+        </div>
         </form>
       </div>
     </div>
